077ae141cd32587cf9fe4f1e859498b539f012fb
BETHA\bruno.kurzawe <compras@4>
2023-05-10 19:05:39 -0300

    teste

---
+++ ExemploGit/src/Teste.java
@@ -0,0 +1,16 @@
+public class Teste {
+
+    private String teste;
+
+    public String getTeste() {
+        return teste;
+    }
+
+    public void setTeste(String teste) {
+        this.teste = teste;
+    }
+
+    public Teste(String teste) {
+        this.teste = teste;
+    }
+}
